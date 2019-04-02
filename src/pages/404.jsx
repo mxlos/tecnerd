6f74ce24816a0574cf668fc7f51d7c6c5d113145
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2019-04-02 08:27:09 -0700

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -11,7 +11,7 @@ const ErrorPage = center => (
     <Header title="404" />
     <Container center={center}>
       <h1>Woops, something went wrong.</h1>
-      <h3>This page does not exist or is no longer reachable.</h3>
+      <h3>This page does not exist or is no longer available.</h3>
       <h3>
         You can return to the <Link to="/">Homepage</Link>.
       </h3>
